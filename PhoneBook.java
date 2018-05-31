581c5fa4d57292ea68e300e2b8d2bf14d5360dba
VanessaNorthway <vanessa.northway@gmail.com>
2018-05-31 07:23:01 -0500

    Create PhoneBook.java

---
+++ PhoneBook.java
@@ -0,0 +1,24 @@
+import java.io.File;
+public class PhoneBook{ 
+
+//initialize a map 
+
+public PhoneBook(){
+
+}
+
+
+public void add(){
+
+}
+
+public Integer getNumber(){
+
+return 0;
+}
+
+public Map printMap(){
+
+}
+
+}
